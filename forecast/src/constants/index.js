53dec5dfc9158a08bc62714002f5f640ca25a565
VitaliMay <mvt30@mail.ru>
2026-03-09 23:32:03 +0300

    feat: add basic layout from nakonsta

---
+++ forecast/src/constants/index.js
@@ -0,0 +1,5 @@
+// openweathermap.org
+export const API_KEY = 'c7bf24a4ff6d678750e63f426cd7fff2'
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
