16aa2638a1c68c3362bfa08ee5450ff9a1f64eba
VitaliMay <mvt30@mail.ru>
2026-03-09 20:38:51 +0300

    init: start forecast project

---
+++ forecast/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import { ref, computed } from 'vue'
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+
+
+
+/**  hover  */
+// @media (hover: hover) and (pointer: fine) {
+//   .weather-form__btn:hover {
+//     background-color: $color-accent;
+//   }
+// }
+
+/** медиазапрос */
+// .weather-info__text {
+//   display: grid;
+//   grid-template-columns: 1fr auto;
+//   gap: 20px;
+//   font-size: 40px;
+
+//   @media (max-width: $mobile-break-point) {
+//     grid-template-columns: 1fr;
+//   }
+// }
+
+</style>
---
+++ forecast/vite.config.js
@@ -0,0 +1,43 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+
+  base: './',
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/assets/styles/_variables.scss" as *;
+          @use "@/assets/styles/_mixins.scss" as *;
+        `
+      }
+    }
+  }
+})
+
+// Чтобы использовать в компонентах style scoped переменные scss
+// css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `
+  //         @use "@/assets/styles/_variables.scss" as *;
+  //         @use "@/assets/styles/_mixins.scss" as *;
+  //       `
+  //     }
+  //   }
+  // }
