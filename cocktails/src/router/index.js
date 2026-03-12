11d48a318043cac613db0661cf46678ffaf76351
VitaliMay <mvt30@mail.ru>
2026-03-12 22:36:44 +0300

    docs: try to fix router and gh-pages

--- cocktails/src/router/index.js
+++ cocktails/src/router/index.js
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/pages/Home.vue'
 import Cocktail from '@/pages/Cocktail.vue'
 import CocktailRandom from '@/pages/CocktailRandom.vue'
@@ -24,7 +25,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(), // вместо с createWebHistory,
   routes
 })
 
